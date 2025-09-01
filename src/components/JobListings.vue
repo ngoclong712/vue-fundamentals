9acd24c919db956820dd764d86204f80fbf93d74
Ngọc Long <143398861+ngoclong712@users.noreply.github.com>
2025-09-01 22:37:49 +0700

    update components - RouterLink

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,7 +1,9 @@
 <script setup>
+    import { RouterLink } from 'vue-router';
     import JobListing from '@/components/JobListing.vue';
-    import jobData from '@/jobs.json';
-    import { ref, defineProps } from 'vue';
+    import { reactive, defineProps, onMounted } from 'vue';
+    import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
+    import axios from 'axios';
 
     defineProps({
         limit: Number,
@@ -11,8 +13,22 @@
         }
     });
 
-    const jobs = ref(jobData)
-    // console.log(jobs.value)
+    const state = reactive({
+        jobs: [],
+        isLoading: true
+    })
+
+    onMounted(async () => {
+        try {
+            const response = await axios.get('http://localhost:5000/jobs');
+            state.jobs = response.data;
+        } catch (error) {
+            console.log("Error fetching jobs", error);
+        }
+        finally {
+            state.isLoading = false;
+        }
+    })
 </script>
 
 <template>
@@ -21,17 +37,23 @@
             <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
                 Browse Jobs
             </h2>
-            <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-                <JobListing v-for="job in jobs.slice(0, limit || jobs.length)" :key="job.id" :job="job" />
+            <!-- Show loading spinner while loading is true -->
+             <div v-if="state.isLoading" class="text-center text-gray-500 py-6">
+                <PulseLoader />
+             </div>
+
+             <!-- Show Job Listing when done loading -->
+            <div v-else class="grid grid-cols-1 md:grid-cols-3 gap-6">
+                <JobListing v-for="job in state.jobs.slice(0, limit || state.jobs.length)" :key="job.id" :job="job" />
             </div>
         </div>
     </section>
 
     <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
-      <a
-        href="/jobs"
+      <RouterLink
+        to="/jobs"
         class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
+        >View All Jobs</RouterLink
       >
     </section>
 </template>
