b0c11b8cca746f9acc7031515551faf39e70e557
Ngọc Long <143398861+ngoclong712@users.noreply.github.com>
2025-09-02 23:14:27 +0700

    update router

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,9 @@ import HomeView from '@/views/HomeView.vue';
 import JobsView from "@/views/JobsView.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
 import JobView from "@/views/JobView.vue";
+import AddJobView from "@/views/AddJobView.vue";
+import EditJobView from "@/views/EditJobView.vue";
+import Test from "@/views/Test.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -27,6 +30,21 @@ const router = createRouter({
             path: '/jobs/:id',
             name: 'job',
             component: JobView
+        },
+        {
+            path: '/jobs/add',
+            name: 'add-job',
+            component: AddJobView,
+        },
+        {
+            path: '/test',
+            name: 'test',
+            component: Test,
+        },
+        {
+            path: '/jobs/edit/:id',
+            name: 'edit-job',
+            component: EditJobView,
         }
     ],
 });
