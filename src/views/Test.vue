4fff1aebbc0ad1628b70fbfed262af3772ab551d
Ngọc Long <143398861+ngoclong712@users.noreply.github.com>
2025-09-02 23:16:47 +0700

    Create Test.vue
    1. Can use router from '@/router' or useRouter()
    2. Async use for function need to wait before moving into next action (await)

---
+++ src/views/Test.vue
@@ -0,0 +1,34 @@
+<script setup>
+    import { useToast } from 'vue-toastification';
+
+    const toast = useToast();
+
+    const showSuccess = () => {
+        console.log('success clicked');
+        toast.success('success');
+    }
+
+    const showError = () => {
+        console.log('error clicked');
+        toast.error('error');
+    }
+</script>
+
+<template>
+    <div class="p-6">
+    <h1 class="text-2xl font-bold mb-4">Test Toast Page</h1>
+    <button 
+      class="px-4 py-2 bg-green-500 text-white rounded mr-4"
+      @click="showSuccess"
+    >
+      Show Success
+    </button>
+
+    <button 
+      class="px-4 py-2 bg-red-500 text-white rounded"
+      @click="showError"
+    >
+      Show Error
+    </button>
+  </div>
+</template>
