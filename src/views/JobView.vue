147dcb4c2f796b23dfd54cadccafdb013a41de74
Ngọc Long <143398861+ngoclong712@users.noreply.github.com>
2025-09-02 23:14:48 +0700

    CRUD Jobs

--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -1,10 +1,14 @@
 <script setup>
   import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
+  import BackButton from '@/components/BackButton.vue';
   import axios from 'axios';
   import { reactive, onMounted } from 'vue';
-  import { useRoute, RouterLink } from 'vue-router'
+  import { useRoute, RouterLink, useRouter } from 'vue-router'
+  import { useToast } from 'vue-toastification';
 
   const route = useRoute();
+  const router = useRouter();
+  const toast = useToast();
 
   const jobId = route.params.id;
 
@@ -13,9 +17,23 @@
     isLoading: true,
   });
 
+  const deleteJob = async () => {
+    try {
+      const confirm = window.confirm('Are you sure you want to delete this job?')
+      if (confirm) {
+        await axios.delete(`/api/jobs/${jobId}`)
+        toast.success("Job Deleted Successfully");
+        router.push('/jobs/')
+      }
+    } catch (error) {
+      console.error("Error deteling job", error);
+      toast.error("Job i not deleted");
+    }
+  }
+
   onMounted (async () => {
     try {
-      const response = axios.get(`http://localhost:5000/jobs/${jobId}`);
+      const response = axios.get(`/api/jobs/${jobId}`);
       state.job = (await response).data;
     } catch (error) {
       console.log("Error fetching job", error);
@@ -26,6 +44,7 @@
 </script>
 
 <template>
+  <BackButton />
   <section v-if="!state.isLoading" class="bg-green-50">
       <div class="container m-auto py-10 px-6">
         <div class="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
@@ -39,7 +58,7 @@
                 class="text-gray-500 mb-4 flex align-middle justify-center md:justify-start"
               >
                 <i
-                  class="fa-solid fa-location-dot text-lg text-orange-700 mr-2"
+                  class="pi pi-map-marker text-lg text-orange-700 mr-2"
                 ></i>
                 <p class="text-orange-700">{{ state.job.location }}</p>
               </div>
@@ -93,7 +112,7 @@
                 class="bg-green-500 hover:bg-green-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 >Edit Job</RouterLink
               >
-              <button
+              <button @click="deleteJob"
                 class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
               >
                 Delete Job
